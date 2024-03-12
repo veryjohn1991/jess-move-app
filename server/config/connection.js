08eea4b616fd6f22113a1d4eb49a95981eb8e0f8
davidmelendez <Dmelz11@github.com>
2024-03-11 17:49:31 -0700

    added a 2 on line 3 for running a test in apollo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/jess-move');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/jess-move2');
 
 module.exports = mongoose.connection;
 
